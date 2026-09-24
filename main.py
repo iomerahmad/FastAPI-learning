1827da69690a312ec19479b3af3ffbc9ba5234f5
Omer Ahmad <soulcraftsucks@gmail.com>
2026-09-24 20:47:30 +0500

    Add users and items endpoints with query parameter experiments.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName) -> dict[str, str]:
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "Message": "Deep learning FTW"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "Message": "leCNN all the images!"}
+    return {"model_name": model_name, "Message": "Have some residuals"}
 
 @app.get("/")
 async def root() -> dict[str, str]:
@@ -17,6 +31,14 @@ async def read_user_me() -> dict[str, str]:
 @app.get("/users/{user_id}")
 async def read_user(user_id: int) -> dict[str, int]:
     return {"user_id": user_id}
+
+@app.get("/items/optional")
+async def optional(q: str | None = None) -> dict[str, str | None]:
+    return {"q": q}
+
+@app.get("/items/required")
+async def required(q: str) -> dict[str, str]:
+    return {"q": q}
 
 
 
